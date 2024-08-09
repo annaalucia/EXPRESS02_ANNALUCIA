ef36623a7981a2ba10e93a2205d199b4c7398fe1
annaalucia <162130074+annaalucia@users.noreply.github.com>
2024-08-09 12:25:32 -0300

    Add files via upload

--- EXPRESS02_ANNALUCIA/server4.js
+++ EXPRESS02_ANNALUCIA/server4.js
@@ -1,43 +1,37 @@
-const express = require("express");
+const express = require('express')
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Olá povo abençoado, servidor no ar");
-});
+app.listen(3000,()=>{
+    console.log("Olá povo abençoado, servidor no ar")
+})
 
 //dados de clientes
 const clientes = [
-  { id: 1, nome: "Altamiro", telefone: "111111111", email: "alta@gmail.com" },
-  {
-    id: 2,
-    nome: "Beringenilda",
-    telefone: "2222222222",
-    email: "berin@gmail.com",
-  },
-  { id: 3, nome: "Conegundes", telefone: "333333333", email: "cne@gmail.com" },
-  { id: 4, nome: "Desidério", telefone: "444444444", email: "desi@gmail.com" },
-  { id: 5, nome: "Emengarda", telefone: "555555555", email: "emen@gmail.com" },
-];
+    {id:1, nome: "Altamiro", telefone: "111111111", email:"alta@gmail.com"},
+    {id:2, nome: "Beringenilda", telefone: "2222222222", email: "berin@gmail.com"},
+    {id:3, nome: "Cenegundes", telefone: "333333333", email:"con@gmail.com"},
+    {id:4, nome: "Desidério", telefone: "444444444", email:"desi@gmail.com"},
+    {id:5, nome: "Emengarda", telefone: "555555555", email:"emen@gmail.com"}
+]
 
 //rotas
-app.get("/", (req, res) => {
-  res.send("API de clientes");
-});
+app.get('/', (req,res)=>{
+    res.send("API de clientes")
+})
 
 //rota2 -  o total de clientes
-app.get("/total_clientes", (req, res) => {
-  res.send("Total de clientes: " + clientes.length);
-});
+app.get('/total_clientes',(req,res)=>{
+    res.send("Total de clientes: "+clientes.length);
+})
 
 //rota3 - apresentar dados de um cliente específico
-app.get("/clientes/:id", (req, res) => {
-  const cliente = clientes.find((c) => c.id === parseInt(req.params.id));
+app.get('/clientes/:id',(req,res)=>{
+    const cliente = clientes.find(c =>c.id === parseInt(req.parmas.id));
 
-  //se o cliente não existe
-  if (!cliente) res.status(404).send("Cliente não encontrado!!!");
+    //se o cliente não existe
+    if(!cliente) res.status(404).send("Cliente não encontrado!!!");
 
-  //se o cliente existe, vamos apresentar uma frase de resposta
-  res.send(
-    `O cliente é: ${cliente.nome}, telefone: ${cliente.telefone}, email: ${cliente.email}`
-  );
-});
+    //se o cliennte existe, vamos apresentar uma frase de resposta
+    res.send(`O cliente é: ${cliente.nome}, telefone: ${cliente.telefone}, email: ${cliente.email}`)
+
+})
--- EXPRESS02_ANNALUCIA/server6.js
+++ EXPRESS02_ANNALUCIA/server6.js
@@ -1,36 +1,57 @@
 //incorporar respostas a partir de outros arquivos
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
-app.listen(3000, () => {
-  console.log("Servidor no ar mano!!!");
-});
+app.listen(3000,()=>{
+    console.log("Servidor no ar mano!!!")
+})
 
 //criar arquivo functions
 
 //importar funções
-const funcoes = require("./functions");
+const funcoes = require('./functions')
 
-app.get("/", (req, res) => {
-  res.send("Teste com arquivos externos");
-});
+app.get('/',(req,res)=>{
+    res.send("Teste com arquivos externos");
+})
 
 //rota para a função add
-app.get("/add/:a/:b", (req, res) => {
-  const a = parseInt(req.params.a);
-  const b = parseInt(req.params.b);
+app.get('/add/:a/:b',(req,res)=>{
+    const a = parseInt(req.params.a);
+    const b = parseInt (req.params.b);
 
-  const resultado = funcoes.add(a, b);
+    const resultado = funcoes.add(a,b);
 
-  res.send(`A soma de ${a} com ${b} é ${resultado}`);
-});
+    res.send(`A soma de ${a} com ${b} é ${resultado}`);
+})
 
-//criar rota para sub (com hífen)
-app.get("/add/:a-:b", (req, res) => {
-  const a = parseInt(req.params.a);
-  const b = parseInt(req.params.b);
+//criar a rota para sub (com hífen)
+app.get('/sub/:a-:b',(req,res)=>{
+    const a = parseInt(req.params.a);
+    const b = parseInt (req.params.b);
 
-  const resultado = funcoes.add(a, b);
+    const resultado = funcoes.sub(a,b);
 
-  res.send(`A sona de ${a} com ${b} é ${resultado}`);
-});
+    res.send(`A subtração de ${a} com ${b} é ${resultado}`);
+})
+
+//rota com divisão
+app.get('/div/:a/:b',(req,res)=>{
+    const a = parseInt(req.params.a);
+    const b = parseInt (req.params.b);
+
+    const resultado = funcoes.div(a,b);
+
+    res.send(`A divisão de ${a} com ${b} é ${resultado}`);
+})
+
+
+//rota com multiplicação
+app.get('/mul/:a/:b',(req,res)=>{
+    const a = parseInt(req.params.a);
+    const b = parseInt (req.params.b);
+
+    const resultado = funcoes.mul(a,b);
+
+    res.send(`A multiplicação de ${a} com ${b} é ${resultado}`);
+})
